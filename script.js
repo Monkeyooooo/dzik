03c582b6117ec5da1dbe0277b5c347bd7d15bff2
Monkeyooooo <107258750+Monkeyooooo@users.noreply.github.com>
2024-10-23 10:13:22 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let i = 0;
+let licznik=0;
+var guzik = false;
+let licznikp= 0;
+let przerwa;
+let p1=document.getElementById("licznik");
+const but = document.getElementById("przycisk");
+let wynik = document.getElementById("wynik");
+function dzik()
+{
+    i++;
+    if(i%7==0 || i.toString().includes('7')==true)
+    {
+         guzik=true;
+         licznikp++;
+    }
+    console.log(i);
+    p1.textContent=i;
+    if((i-1)%7==0 || (i-1).toString().includes('7')==true)
+    {
+        if(licznikp!=licznik)
+        {
+            clearInterval(przerwa);
+            alert("Przegrałeś!");
+        }
+    }
+   
+}
+
+function przyciskFunc()
+{ 
+    console.log(but);
+    but.addEventListener("click",function(){
+        if(guzik==true){
+            licznik++;
+            
+        }
+        if(guzik==false){
+            clearInterval(przerwa);
+            alert("Przegrałeś!");
+            
+        }
+        console.log("chuj");
+        guzik = false
+        wynik.textContent=licznik;
+    }
+    )};
+but.addEventListener("click", przyciskFunc());
+przerwa = setInterval(dzik,1000);
